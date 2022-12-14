62cce5742857f7104e00bef56f8cf266c5710b00
mattiabenelli <benellimattia0@gmail.com>
2022-12-14 18:54:19 +0100

    milestone 0 fatta

---
+++ js/script.js
@@ -0,0 +1,27 @@
+const game = [
+    {
+        img: "01.webp",
+        nome: "spiderman",
+        descrizione: "loremipsumdolor",
+    },
+    {
+        img: "02.webp",
+        nome: "spiderman",
+        descrizione: "loremipsumdolor",
+    },     
+    {
+        img: "03.webp",
+        nome: "spiderman",
+        descrizione: "loremipsumdolor",
+    }, 
+    {
+        img: "04.webp",
+        nome: "spiderman",
+        descrizione: "loremipsumdolor",
+    },
+    {
+        img: "05.webp",
+        nome: "spiderman",
+        descrizione: "loremipsumdolor",
+    } 
+]
